01c41f46ca2f27b079f770a8db80b33bcbc06141
rodrigolayme199629 <37313083+rodrigolayme199629@users.noreply.github.com>
2018-05-14 18:24:50 -0500

    Lab10

---
+++ lab10/lab10/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
+
+import classes from './App.css';
+
+import Posts from './containers/Posts/Posts';
+import NewPost from './components/NewPost/NewPost';
+class App extends Component {
+  render() {
+    return (
+            <BrowserRouter>
+            <div className={classes.App}>
+              <header>
+                <nav>
+                  <ul>
+                    <li><NavLink
+                      to="/"
+                      exact
+                      activeStyle={{
+                      color: '#fa923f',
+                      textDecoration: 'underline'
+                    }}>Inicio</NavLink>
+                    </li>
+                    <li><NavLink
+                      to="/posts"
+                      exact
+                      activeStyle={{
+                      color: '#fa923f',
+                      textDecoration: 'underline'
+                    }}>Posts</NavLink>
+                    </li>
+                    <li><NavLink
+                      to={{
+                      pathname: '/new-post',
+                      hash: '#submit',
+                      search: '?quick-submit=true'
+                    }}>New Post
+                      </NavLink>
+                    </li>
+                  </ul>
+                </nav>
+              </header>
+              <h1>Blog de ejemplo</h1>
+              <Switch>
+                <Route path="/new-post" component={NewPost} />
+                <Route path="/posts" component={Posts} />
+                <Route path="/" exact render={() => <h1>Página de inicio</h1>}/>
+                <Route render={() => <h1>Not found</h1>}/>
+              </Switch>
+            </div>
+            </BrowserRouter>
+
+    );
+  }
+}
+
+export default App;
